7a785c200155736e946c88430314c41330a31edd
Emanuel Camarena <emanuel.ortega@hotmail.com>
2022-11-14 20:07:49 -0600

    Added function with return

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-while(true){
-    let input_alert = prompt("Ingrese su nombre: ")
-    if(input_alert == " "){
-        alert("Ingrese un usuario valido")
-    }else if(input_alert == null || input_alert == ""){
-        alert("Usted no ha ingresado ningun usuario, intente de nuevo")
-    }else{
-        alert("Bienvenido usuario: " + input_alert)
-        break
+let userName = function(){
+    while(true){
+        let input_alert = prompt("Ingrese su nombre: ")
+        if(input_alert == " "){
+            alert("Ingrese un usuario valido")
+        }else if(input_alert == null || input_alert == ""){
+            alert("Usted no ha ingresado ningun usuario, intente de nuevo")
+        }else{
+            return input_alert
+        }
     }
 }
+
+alert("Bienvenido usuario: " + userName())
