0de53913b43be7c7d80ab445313087d90fe84eaf
Emanuel Camarena <emanuel.ortega@hotmail.com>
2022-11-28 20:55:55 -0600

    Removed entrega 1, missing to add .find .filter functions

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-let userName = function(){
-    while(true){
-        let input_alert = prompt("Ingrese su nombre: ")
-        if(input_alert == " "){
-            alert("Ingrese un usuario valido")
-        }else if(input_alert == null || input_alert == ""){
-            alert("Usted no ha ingresado ningun usuario, intente de nuevo")
-        }else{
-            return input_alert
-        }
+class Pokemon{
+    constructor(id,name){
+        this.id = id;
+        this.name = name;
     }
 }
 
-alert("Bienvenido usuario: " + userName())
+let bulbasaur = new Pokemon(1,"Bulbasaur")
+let charmander = new Pokemon(2,"Charmander")
+let squirtle = new Pokemon(3,"Squirtle")
+
+let pokemones = [bulbasaur, charmander, squirtle]
+
