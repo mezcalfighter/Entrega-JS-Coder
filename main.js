d9bd8c86f87076f915b09c8d2ec61078c39e57a4
Emanuel Camarena <emanuel.ortega@hotmail.com>
2022-11-29 20:47:41 -0600

    Added find option

--- main.js
+++ main.js
@@ -5,9 +5,30 @@ class Pokemon{
     }
 }
 
-let bulbasaur = new Pokemon(1,"Bulbasaur")
-let charmander = new Pokemon(2,"Charmander")
-let squirtle = new Pokemon(3,"Squirtle")
+let bulbasaur = new Pokemon(1,"Bulbasaur");
+let charmander = new Pokemon(2,"Charmander");
+let squirtle = new Pokemon(3,"Squirtle");
 
-let pokemones = [bulbasaur, charmander, squirtle]
+let pokemones = [bulbasaur, charmander, squirtle, bulbasaur];
 
+// Find
+const submitSearch = document.getElementById("search-btn");
+submitSearch.addEventListener("click",(e) => {
+    e.preventDefault();
+    const elementHTML = document.getElementById("name-input").value;
+    const lookFor = pokemones.find(element => element.name == elementHTML);
+    if(lookFor){
+        alert(`${lookFor.name} si existe`);
+    }else{
+        alert(`${elementHTML} no existe`);
+    }
+});
+
+// Filter
+const submitFilter = document.getElementById("search-btn2");
+submitFilter.addEventListener("click",(e) => {
+    e.preventDefault();
+    const elementHTML = document.getElementById("name-input-filter").value;
+    const lookFor = pokemones.find(element => element.name == elementHTML);
+    console.log(lookFor);
+});
