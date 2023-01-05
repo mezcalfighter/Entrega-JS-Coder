455a14dc53bd3bef63e9ad83188986544340632a
Emanuel Camarena <emanuel.ortega@hotmail.com>
2023-01-04 22:23:04 -0600

    Fixed right side, working pokedex

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 class Pokemon{
-    constructor(id,name,sprites){
+    constructor(id,name,sprites,height,weight,type){
         this.id = id;
         this.name = name;
         this.sprites = sprites
+        this.height = height
+        this.weight = weight
+        this.type = type
     }
 }
 
@@ -12,6 +15,36 @@ let pokemon = new Pokemon()
 const submitSearch = document.getElementById("search-btn");
 submitSearch.addEventListener("click",(e) => {
     e.preventDefault();
+    GetPokemon()
+});
+
+const submitInput = document.getElementById("name-input")
+submitInput.addEventListener("keypress",(e) => {
+    e.preventDefault();
+    e.key == "Enter" ? GetPokemon() : submitInput.value += e.key
+});
+
+
+if(localStorage.key("pokemonStorage")){
+    let pokemonJSON = localStorage.getItem("pokemonStorage");
+    let pokemon = JSON.parse(pokemonJSON);
+    InsertPokemon(pokemon);
+}
+
+function InsertPokemon(pokemon){
+    const nameScreen = document.getElementById("name-screen");
+    const mainScreen = document.getElementById("main-screen");
+    const aboutScreen = document.getElementById("about-screen");
+    const idScreen = document.getElementById("id-screen");
+    const typeScreen = document.getElementById("type-screen");
+    nameScreen.innerText = `${pokemon.name}`;
+    aboutScreen.innerText = `Height: ${pokemon.height * 10}cm Weight: ${pokemon.weight / 10}kg`
+    idScreen.innerText = `# ${pokemon.id}`
+    typeScreen.innerText = `${pokemon.type}`
+    mainScreen.style.backgroundImage = `url(${pokemon.sprites})`;
+}
+
+function GetPokemon(){
     const elementHTML = document.getElementById("name-input").value;
     let pokemonAPI = `https://pokeapi.co/api/v2/pokemon/${elementHTML.toLowerCase()}`
     const nameScreen = document.getElementById("name-screen");
@@ -22,6 +55,10 @@ submitSearch.addEventListener("click",(e) => {
             pokemon.id = data.id
             pokemon.name = data.name
             pokemon.sprites = data.sprites.front_default
+            pokemon.height = data.height
+            pokemon.weight = data.weight
+            pokemon.type = data.types[0].type.name
+            console.log(pokemon)
             localStorage.setItem("pokemonStorage",JSON.stringify(pokemon));
             InsertPokemon(pokemon)
         })
@@ -38,22 +75,7 @@ submitSearch.addEventListener("click",(e) => {
             mainScreen.style.removeProperty("background-image");
             localStorage.clear();
         })
-});
-
-
-if(localStorage.key("pokemonStorage")){
-    let pokemonJSON = localStorage.getItem("pokemonStorage");
-    let pokemon = JSON.parse(pokemonJSON);
-    InsertPokemon(pokemon);
 }
-
-function InsertPokemon(pokemon){
-    const nameScreen = document.getElementById("name-screen");
-    const mainScreen = document.getElementById("main-screen");
-    nameScreen.innerText = `${pokemon.name}`;
-    mainScreen.style.backgroundImage = `url(${pokemon.sprites})`;
-}
-
 // Filter - REMOVED
 // const submitFilter = document.getElementById("search-btn2");
 // submitFilter.addEventListener("click",(e) => {
